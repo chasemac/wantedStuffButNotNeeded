bda10dea6a84f5d2e4d11f52a138dbe85ed00f6c
Chase McElroy <ichasem@gmail.com>
2016-07-12 15:48:30 -0700

    connected storyboard to VC

---
+++ wantedStuffButNotNeeded/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ListCell.swift
+//  wantedStuffButNotNeeded
+//
+//  Created by Chase McElroy on 7/12/16.
+//  Copyright © 2016 Chase McElroy. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+
+}
--- wantedStuffButNotNeeded/MainVC.swift
+++ wantedStuffButNotNeeded/MainVC.swift
@@ -8,17 +8,31 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segment: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 0
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
 
 
 }
